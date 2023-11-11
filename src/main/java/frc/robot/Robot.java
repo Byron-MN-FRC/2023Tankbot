38ea3c977d95164b03a835a862442577eb2cbd49
PAPA_FAWSON <ammonfawson@live.com>
2023-11-10 20:09:03 -0600

    added cameras

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import org.photonvision.PhotonCamera; 
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
